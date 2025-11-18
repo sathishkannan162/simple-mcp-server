cb1d003f51c28fe04f93e6931ef8bb17434b7673
Sathish Kannan <sathishkannan162@gmail.com>
2025-11-18 15:51:42 +0530

    Initial commit

---
+++ app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 4000;
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
