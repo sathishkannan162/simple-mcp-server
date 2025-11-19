7523f813c28f42a884b7cf24ab27ecb4d480eb13
Sathish Kannan <sathishkannan162@gmail.com>
2025-11-19 09:47:20 +0530

    Add resource and resource templte for table schemas

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,95 @@
+/** biome-ignore-all lint/suspicious/noExplicitAny: mcp types files are not correct */
 import {
   McpServer,
   ResourceTemplate,
 } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+import dotenv from "dotenv";
 import express from "express";
+import pg from "pg";
 import { z } from "zod";
+
+dotenv.config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const pool = new pg.Pool({
+  connectionString: DATABASE_URL,
+});
+console.log(DATABASE_URL, "env");
+const SCHEMA_PATH = "schema";
 
 // Create an MCP server
 const server = new McpServer({
   name: "demo-server",
   version: "1.0.0",
 });
+
+// @ts-expect-error types bad.
+server.registerResource(
+  "Table Schema",
+  new ResourceTemplate("table://{tableName}/schema", {
+    // @ts-expect-error: types bad
+    list: async () => {
+      const client = await pool.connect().catch((err) => {
+        console.log(err);
+        return err;
+      });
+      try {
+        const result = await client.query(
+          "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+        );
+
+        return {
+          resources: result.rows.map((row: any) => {
+            const tableName = row.table_name;
+            return {
+              uri: `table://${tableName}/schema`,
+              mimeType: "application/json",
+              name: `"${tableName}" database schema`,
+            };
+          }),
+        };
+      } catch (err) {
+        console.error("err", err);
+      } finally {
+        client.release();
+      }
+    },
+  }) as unknown as string,
+  {
+    title: "Table schemas",
+    description: "Individual Table Schema",
+  },
+  async (uri, { tableName }: { tableName: string }) => {
+    const pathComponents = uri.href.split("/");
+    const schema = pathComponents.pop();
+    console.log("table", tableName);
+
+    if (schema !== SCHEMA_PATH) {
+      throw new Error("Invalid resource URI");
+    }
+
+    const client = await pool.connect();
+    try {
+      const result = await client.query(
+        "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = $1",
+        [tableName]
+      );
+
+      return {
+        contents: [
+          {
+            uri: uri.href,
+            mimeType: "application/json",
+            text: JSON.stringify(result.rows, null, 2),
+          },
+        ],
+      };
+    } finally {
+      client.release();
+    }
+  }
+);
 
 // Add an addition tool
 server.registerTool(
